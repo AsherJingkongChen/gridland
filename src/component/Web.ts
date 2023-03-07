b929005505a0461bcb820d7f3ff66214cfc0b1d1
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2023-03-07 13:45:30 +0800

    To wrap life cycle of components

---
+++ src/component/Web.ts
@@ -0,0 +1,14 @@
+export class Web {
+  public canvas?: Application;
+  public camera?: Camera;
+  public profiler?: Profiler;
+  public zone?: Zone;
+
+  public openWorld(worldid: number): void {
+    
+  }
+
+  public closeWorld(): void {
+    
+  }
+}
